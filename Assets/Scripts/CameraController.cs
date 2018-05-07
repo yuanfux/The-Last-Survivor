87b128a5f12527afbadcf62bc94cba15e3a06549
Yuan Fu <fuyuanx@gmail.com>
2018-05-07 22:37:27 +0800

    basic camera setup

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform target;
+
+    public float smoothSpeed = 10f;
+
+    public Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        Vector3 dest = target.position + offset;
+        Vector3 smoothDest = Vector3.Lerp(transform.position, dest, smoothSpeed * Time.deltaTime);
+        transform.position = smoothDest;
+	}
+}
